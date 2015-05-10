92d4e80cdd906d8b77280914b943728b138b5bb5
aman-ahluwalia <aman.explorer@gmail.com>
2015-05-10 11:43:10 -0700

    General Algos

--- spoj_sol/KAMIL.cpp
+++
@@ -1,2 +0,0 @@
-#include<iostream> 
-using namespace std;int main(){int t=10;while(t--){string s;cin>>s;int c=1,l=s.length()-1;while(l>=0){if(s[l]=='T'||s[l]=='D'||s[l]=='L'||s[l]=='F') c *= 2;l--;} cout << c << endl;}}
--- spoj_sol/SIZECON.cpp
+++
@@ -1,2 +0,0 @@
-#include<iostream>
-using namespace std;int main(){int t,n,s=0;cin>>t;while(t--){cin>>n;if(n>0)s+=n;}cout<<s;}
